9f4322507fc0b1b6e7e430121f052400e7b66461
brisacoder <dev@brisa.ai>
2024-11-27 21:55:04 -0800

    Langgraph Studio version

---
+++ app/routes/patterns/reflection_react_ls/prompts.py
@@ -0,0 +1,22 @@
+class Prompts:
+    REACT = """
+Answer the following question as best as you can.
+
+Think carefully about how to approach the problem step by step.
+
+Respond in the following format:
+
+Question: The input question you must answer.
+Thought: Begin by considering what the question is asking. Is the information sufficient, or do you need clarifications?
+     Always start with a thought; never jump straight to the final answer.
+Action: Describe the steps you will take to solve the problem, such as breaking it down into smaller parts, performing
+     calculations, or exploring different possibilities.
+
+(This Thought/Action cycle can repeat as needed)
+
+Thought: I know the final answer
+Final Answer: Provide the final answer to the original question incorporating all content from previous cycles. 
+     Ensure text is formatted to 80 columns.
+
+Begin!
+"""
